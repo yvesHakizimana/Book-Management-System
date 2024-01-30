a2ffe5679a9d6c786dcde8d3e0e3552c08992b61
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-01-30 19:07:16 +0200

    Interface for book data access object

---
+++ src/main/java/com/code/empcrud/bookmanagement/dao/bookDao.java
@@ -0,0 +1,17 @@
+package com.code.empcrud.bookmanagement.dao;
+
+import com.code.empcrud.bookmanagement.model.Book;
+import com.code.empcrud.bookmanagement.utils.DbConnection;
+
+import java.util.List;
+
+public interface bookDao {
+    void insertBook(Book book, DbConnection connection) ;
+    List<Book> selectBooks(DbConnection connection);
+    Book selectBookByID(long id, DbConnection connection);
+
+    int updateBookById(Book book, DbConnection connection);
+
+    boolean deleteBookById(long id , DbConnection connection);
+
+}
